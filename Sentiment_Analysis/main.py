1a5d94c21268bd4778ee0ff0da52d7a9e25e09d8
tmgreenside <trevmg7@gmail.com>
2019-06-29 00:54:17 -0600

    Working on logistic regression

--- Sentiment_Analysis/main.py
+++ Sentiment_Analysis/main.py
@@ -5,14 +5,19 @@ Sentiment labels:
 """
 import sys, os
 from models.ClassifierFeedFwd import ClassifierFeedFwd
+from models.ClassifierLogReg import ClassifierLogReg
 
 from logger import *
 from models import ClassifierFeedFwd
 
 def experiment():
-    logger.log("Working with Feed Forward model")
-    modelFwd = ClassifierFeedFwd.ClassifierFeedFwd()
-    modelFwd.experiment()
+    # logger.log("Working with Feed Forward model.")
+    # modelFwd = ClassifierFeedFwd.ClassifierFeedFwd()
+    # modelFwd.experiment()
+
+    logger.log("Working with the Logistic Regression model.")
+    modelLogReg = ClassifierLogReg()
+    modelLogReg.experiment()
 
 if __name__ == "__main__":
     try:
--- Sentiment_Analysis/models/ClassifierLogReg.py
+++ Sentiment_Analysis/models/ClassifierLogReg.py
@@ -5,15 +5,21 @@ from sklearn.model_selection import train_test_split
 from . import Classifier
 from logger import *
 
-class ClassifierFeedFwd(Classifier.Classifier):
+class ClassifierLogReg(Classifier.Classifier):
     def __init__(self):
-        pass
+        self.model = self.buildModel()
+        logger.log("Logistic regression model build.")
 
     def buildModel(self):
-        pass
+        model = LogisticRegression(C=0.05)
+        return model
 
     def getModel(self):
-        return model
+        return self.model
 
     def experiment(self):
         (x_train, y_train), (x_test, y_test) = self.getData()
+        self.model.fit(x_train, y_train)
+        acc = accuracy_score(y_test, model.predict(x_test))
+        logger.log("Accuracy: {}".format(acc))
+        return
