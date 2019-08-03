9bf99671743d1c3df5f7cd400cc6b016fc2727b5
tmgreenside <trevmg7@gmail.com>
2019-08-03 17:17:38 -0600

    Added starter files for the FaceNet project

---
+++ FaceNet/model.py
@@ -0,0 +1,10 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+
+class Model:
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        model = Sequential()
+        return model
